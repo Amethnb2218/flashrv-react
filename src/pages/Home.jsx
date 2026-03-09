4f855c5fb4e7a6484e7fca3e7b028ff59398b228
Mouhamed Sall <amethsl2218@gmail.com>
2026-03-09 19:29:59 +0000

    hero: aligner texte et image en haut (items-start)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -195,7 +195,7 @@ function Home() {
           <div className="absolute bottom-10 left-8 w-80 h-80 bg-yellow-100/40 rounded-full blur-3xl"></div>
         </div>
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-3 pb-4">
-          <div className="grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-start">
             <motion.div
               initial={{ opacity: 0, y: 24 }}
               animate={{ opacity: 1, y: 0 }}
